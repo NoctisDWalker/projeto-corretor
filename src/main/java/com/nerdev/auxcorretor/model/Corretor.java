aaf2d233b0cf13e02cb0812fe48ff974f377be31
Filipe dos Santos <filipeuvft@gmail.com>
2026-03-27 15:35:05 -0300

    Criar Corretor com Auditoria e Validações Iniciais

---
+++ src/main/java/com/nerdev/auxcorretor/model/Corretor.java
@@ -0,0 +1,48 @@
+package com.nerdev.auxcorretor.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "corretor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Corretor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ToString.Exclude
+    @Column(nullable = false)
+    private String senha;
+
+    @Column
+    private String telefone;
+
+    @Column(nullable = false)
+    private String creci;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean ativo = true;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDate dataCadastro;
+}
