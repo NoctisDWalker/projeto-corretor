b559ee1dec6000f6fb2ef366adafd0011aecc5b1
Filipe dos Santos <filipeuvft@gmail.com>
2026-03-30 11:54:02 -0300

    Criação das Primeiras Exceções de Dominio

---
+++ src/main/java/com/nerdev/auxcorretor/exception/DuplicateEntityException.java
@@ -0,0 +1,7 @@
+package com.nerdev.auxcorretor.exception;
+
+public class DuplicateEntityException extends BusinessException {
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/nerdev/auxcorretor/exception/RequiredFieldException.java
@@ -0,0 +1,14 @@
+package com.nerdev.auxcorretor.exception;
+
+import lombok.Getter;
+
+public class RequiredFieldException extends BusinessException{
+
+    @Getter
+    private String campo;
+
+    public RequiredFieldException(String message, String campo) {
+        super(message);
+        this.campo = campo;
+    }
+}
