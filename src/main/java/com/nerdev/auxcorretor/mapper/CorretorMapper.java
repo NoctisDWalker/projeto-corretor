58b59f39e19b600dbe324659ed20cffee0ffc5ab
Filipe dos Santos <filipeuvft@gmail.com>
2026-03-31 15:35:18 -0300

    Ajustes no Pom, docker-compose, CorretorMapper e application.yaml Para Teste Completo

--- src/main/java/com/nerdev/auxcorretor/mapper/CorretorMapper.java
+++ src/main/java/com/nerdev/auxcorretor/mapper/CorretorMapper.java
@@ -7,7 +7,7 @@ import com.nerdev.auxcorretor.model.Corretor;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper
+@Mapper(componentModel = "Spring")
 public interface CorretorMapper {
     Corretor toEntity(CorretorCreateRequestDTO dto);
     void updateEntity(@MappingTarget Corretor Entity, CorretorUpdateRequestDTO dto);
