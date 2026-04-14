c4ca83b0183e40806c7416bce81f9364c3e9cb87
Filipe dos Santos <filipeuvft@gmail.com>
2026-04-14 14:41:12 -0300

    Melhoria do StatusVisitaEnum

--- src/main/java/com/nerdev/auxcorretor/model/enums/StatusVisitaEnum.java
+++ src/main/java/com/nerdev/auxcorretor/model/enums/StatusVisitaEnum.java
@@ -1,11 +1,32 @@
 package com.nerdev.auxcorretor.model.enums;
 
+import java.util.List;
+
 public enum StatusVisitaEnum {
 
-    AGENDADA,
-    CONFIRMADA,
-    REALIZADA,
-    CANCELADA,
-    NAO_COMPARECEU
+    AGENDADA(false),
+    CONFIRMADA(false),
+    REMARCADA(false),
+    REALIZADA(true),
+    CANCELADA(true),
+    NAO_COMPARECEU(true);
+
+    private final boolean finalizado;
+
+    StatusVisitaEnum(boolean finalizado){
+        this.finalizado = finalizado;
+    }
+
+    public boolean isFinalizado() {
+        return finalizado;
+    }
+
+    public boolean isAtivo() {
+        return !finalizado;
+    }
+
+    public static List<StatusVisitaEnum> statusFinais(){
+        return List.of(REALIZADA, CANCELADA, NAO_COMPARECEU);
+    }
 
 }
